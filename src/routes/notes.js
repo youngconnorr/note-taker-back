c002f47ce3e7a9176e1324ec3813f3b3f88b976c
conryxng <youngconnor2004@gmail.com>
2023-08-17 14:43:43 -0600

    created route for notes

--- src/routes/notes.js
+++ src/routes/notes.js
@@ -4,4 +4,24 @@ import mongoose from 'mongoose';
 
 const router = express.Router();
 
+router.get("/", async(req,res) => {    
+    try {
+        const response = await NotesModel.find({}); // goes through model finding what is requesting for response
+        res.json(response)
+    } catch(error) {
+        res.json(err);
+    }
+})
+
+router.post("/add", async(req,res) => {
+    const note = new NotesModel(req.body) //means that the new note will be the requests body
+    
+    try {
+        await note.save(); // goes through model finding what is requesting for response
+        res.json(note)
+    } catch(error) {
+        res.json(err);
+    }
+})
+
 export { router as notesRouter}
