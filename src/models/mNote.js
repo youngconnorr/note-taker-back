0b5d5dbaa115ef9515693cfbeac8fc79f401a17d
conryxng <youngconnor2004@gmail.com>
2023-08-24 16:56:33 -0600

    fixed userID

--- src/models/mNote.js
+++ src/models/mNote.js
@@ -4,6 +4,11 @@ const NotesSchema = new mongoose.Schema({
     color: { type: String, required: true },
     name: { type: String, required: true, },
     content: { type: String, required: true },
+    userOwner: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
 });
 
 export const NotesModel = mongoose.model("notes", NotesSchema);
